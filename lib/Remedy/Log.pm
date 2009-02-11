76a7de24426791a8724c46e6ce5f2cdfaea12610
Tim Skirvin <tskirvin@stanford.edu>
2009-02-11 13:21:26 -0800

    Log.pm is all fixed up and pretty

--- lib/Remedy/Log.pm
+++ lib/Remedy/Log.pm
@@ -7,32 +7,46 @@ Remedy::Log - Remedy logging functions
 
 =head1 SYNOPSIS
 
-    use Remedy::Config;
-    use Log::Log4perl qw/:easy/;
+    use Remedy::Log;
 
-    my $logger = get_logger ('');   # get the root level logger
+    my $logger = Remedy::Log->get_logger ();
     $logger->debug ('If this shows anywhere, we have a problem.');
 
-    my $file = '/etc/remedy/remedy.conf';
-    my $config = Remedy::Config->load ($file);
-
-    ## NOT NECESSARY, but helpful to illustrate what's going on
-    # $logger = $config->logger;  
+    my $log = Remedy::Log->new (
+        'file'       => '/tmp/remedy-log.txt',
+        'level'      => $Log::Log4perl::ERROR,
+        'level_file' => $Log::Log4perl::INFO,
+    );
+    $log->init ();
 
     $logger->warn ('You should see this on STDERR and in the logfile.');
     $logger->info ('This should only appear in the logfile.');
-    $logger->more_logging (1);
+    $log->more_logging (1);
     $logger->info ('Now this should also appear on STDERR.');
 
 =head1 DESCRIPTION
 
-Remedy::Log manages the logging functions for B<Remedy>, using several
-root-level loggers implemented using B<Log::Log4perl>.  This controls the
-status and error messages sent to both STDERR, and (optionally) to a central
-log file.
+Remedy::Log manages the logging functions for B<Remedy>, using two root-level
+loggers implemented using B<Log::Log4perl>:
 
-Remedy::Log is implemented as a B<Class::Struct> object, and is initialized 
-via B<Remedy::Config>.
+=over 4
+
+=item screen
+
+Output is logged to STDERR using a I<ScreenColoredLevels> appender.
+
+=item file
+
+Output is appended to a defined, central log file.
+
+=back
+
+Once configured, scripts and modules should send copious debugging information
+to the root logger, and B<Log::Log4perl> will take care of deciding what to do
+with it.
+
+Remedy::Log is implemented as a B<Class::Struct> object, and is initialized via
+B<Remedy::Config>.
 
 =cut
 
@@ -97,19 +111,14 @@ to record levels I<info>, I<warn>, I<error>, and I<fatal>.
 
 =cut
 
-our $LOGLEVEL_FILE = $INFO;
-
-=item $NAME
-
-necessary?
-
-=cut
-
-our $NAME = "Remedy";
+our $LOGLEVEL_FILE = $Log::Log4perl::INFO;
 
 =back
 
 =cut
+
+## Global Configuration
+$Log::Log4perl::one_message_per_appender = 1;
 
 ##############################################################################
 ### Declarations
@@ -119,7 +128,7 @@ use strict;
 use warnings;
 
 use Class::Struct;
-use Log::Log4perl qw/:easy/;
+use Log::Log4perl qw/:nowarn/;
 use Log::Log4perl::Level;
 
 ##############################################################################
@@ -130,18 +139,25 @@ use Log::Log4perl::Level;
 
 =head2 B<Class::Struct> Accessors
 
-As noted above, most subroutines are handled by B<Class::Struct>; please see
-its man page for more details about the various sub-functions.
+These fields can be initialized via B<new ()> or per-function.  
 
 =over 4
 
 =item file ($)
 
+Filename for file output.  Corresponds to I<$FILE>.
+
 =item level ($)
+
+Loglevel for STDERR output.  Corresponds to I<$LOGLEVEL>.
 
 =item level_file ($)
 
+Loglevel for file output.  Corresponds to I<$LOGLEVEL_FILE>.
+
 =item logger (Log::Log4perl::Logger)
+
+The actual B<Log::Log4perl::Logger> object.
 
 =back
 
@@ -152,69 +168,126 @@ struct 'Remedy::Log' => {
     'level'      => '$',
     'level_file' => '$',
     'logger'     => 'Log::Log4perl::Logger',
-    'name'       => '$',
 };
-
-=back
 
 =head2 Additional Functions
 
 =over 4
 
+=item get_logger ()
+
+If invoked on a configured object, returns the value of B<logger ()>.  If
+invoked from the module itself or from an uninitialized object, then we will
+return the root-level logger.
+
+=cut
+
+sub get_logger { 
+    my ($self) = @_;
+    return $self->logger if (ref $self && $self->logger);
+    return Log::Log4perl::get_logger ('');
+}
+
 =item init ()
+
+(Re)initializes the B<Remedy::Log> object.  This follows the general path of:
+
+    Pull in default values, if not already set
+    Get default logger object (defaults to root-level)
+    Blow away existing appenders, if any
+    Initialize screen appender, add to our logger object
+    Initialize file appender (if applicable), add to our logger object
+    Set logger level to the higher of the two log levels
+    Save logger with logger () accessor, and return
+
+In order to set up basic logging immediately, this is invoked on module load
+with default options.
 
 =cut
 
 sub init {
-    warn "INIT: @_\n";
     my ($self, %args) = @_;
+    $self = $self->new unless ref $self;
 
-    $self->name       ($self->name       || $NAME);
-    $self->level      ($self->level      || $LOGLEVEL);
-    $self->level_file ($self->level_file || $LOGLEVEL_FILE);
-    $self->file       ($self->file       || $FILE);
 
-    foreach (qw/name level level_file file/) {
-        warn "F: $_ " . $self->$_ . "\n";
-    }
-    my (%appenders, %layouts);
+    $self->level      ($LOGLEVEL)      unless defined $self->level;
+    $self->level_file ($LOGLEVEL_FILE) unless defined $self->level_file;
+    $self->file       ($FILE)          unless defined $self->file;
 
-    # Define a category logger
-    my $logger = Log::Log4perl->get_logger ('');
-    $Log::Log4perl::one_message_per_appender = 1;
+    ## Get the Log::Log4perl::Logger object we'll use consistently
+    my $logger = $self->get_logger;
 
+    ## Clear the old appenders
+    foreach my $old (qw/screen file/) { $logger->eradicate_appender ($old) }
+
+    ## Create the screen appender
+    $logger->all ('adding screen appender');
     my $appender =  Log::Log4perl::Appender->new (  
         "Log::Log4perl::Appender::ScreenColoredLevels", 
-        'name' => "$0", stderr => 1);
+        'name' => "screen", stderr => 1);
     my $layout = Log::Log4perl::Layout::PatternLayout->new ($FORMAT);
     $appender->layout ($layout);
-
+    $logger->all ("set screen appender threshold to " .  $self->level);
+    $appender->threshold ($self->level);
     $logger->add_appender ($appender);
+    binmode (STDERR, ":utf8");
 
     if (my $name = $self->file) {
+        $logger->all ("adding file appender to $name");
         my $appender = Log::Log4perl::Appender->new (
             "Log::Log4perl::Appender::File",
-            filename  => $self->file);
+            'name' => 'file', 'filename' => $name, 'utf8' => 1);
         my $layout = Log::Log4perl::Layout::PatternLayout->new 
-            ($FORMAT_FILE . $FORMAT);
+            ($FORMAT_FILE);
         $appender->layout ($layout);
 
+        $logger->all ("set file appender threshold to " .  $self->level_file);
         $appender->threshold ($self->level_file);
         $logger->add_appender ($appender);
     }
 
-    $logger->level ($self->level);
+    my $level = $self->level_file >= $self->level ? $self->level_file 
+                                                  : $self->level;
+    $logger->all ('set global appender level to' . $level);
+    $logger->level ($level);
     $self->logger ($logger);
 }
 
-=item more_logging (COUNT)
+## Initialize default logging 
+__PACKAGE__->init ();
+
+=item more_logging (COUNT [, APPENDERS])
+
+Increases the amount of logging by I<COUNT> levels.  This increases the
+loglevel with B<Log::Log4perl::more_logging ()>, and decreases the appender
+thresholds with B<Log::Log4perl::appender_thresholds_adjust ()>.
+
+If an arrayref I<APPENDERS> is offered, then this latter part we will adjust
+the thresholds of the appenders named after the array's contents; if not, we
+will just adjust the threshold for 'screen'.
 
 =cut
 
 sub more_logging {
-    my ($self, $count) = @_;
+    my ($self, $count, $appender) = @_;
+    my $logger = $self->logger || return;
+
     return unless $count > 0;
-    $self->logger->more_logging (int $count);
+    if ($appender && ! ref $appender) {
+        $logger->debug ('appender must be an arrayref');
+        $appender = [$appender];
+    } else { $appender ||= ['screen']; }
+
+    $logger->all ("increasing global loglevel by $count");
+    $logger->more_logging (int $count);
+    
+    $logger->all (sprintf ("adjusting appender thresholds by %d for %s",
+        int $count, join (', ', @$appender)));
+    Log::Log4perl->appender_thresholds_adjust (- int $count, $appender);
+
+    $logger->debug (sprintf ('increased loglevel by %d for %s', 
+        int $count, join (', ', @$appender)));
+    return 1;
 }
 
 =back
@@ -222,12 +295,101 @@ sub more_logging {
 =cut
 
 ##############################################################################
-### Final Documentation
+### Final Documentation ######################################################
 ##############################################################################
+
+=head1 Log::Log4perl Overview
+
+While I have no desire to reproduce the B<Log::Log4perl> man page, a short
+introduction to using the module might be helpful.  (Parts of this are cribbed
+from the B<Log::Log4perl> manual page.)
+
+The main object that you will interact with is a B<Log::Log4perl::Logger>
+object (B<$logger>).  This object offers a variety of priority logging functions:
+
+    $logger->all   ()    # incorrectly listed as 'trace' in the man page
+    $logger->debug () 
+    $logger->info  ()  
+    $logger->warn  ()  
+    $logger->error () 
+    $logger->fatal () 
+
+Each of these functions corresponds to a priority.  When the function is
+called, we compare this priority to the loglevel set within $logger; if the
+loglevel at least matches the priority, then we'll print the message.  So, if
+the loglevel is set to, say, B<$Log::Log4level::WARN>, then we will print
+messages whenever we call B<warn ()>, B<error ()>, or B<fatal ()>.
+
+There are two ways to look at this state of affairs:
+
+=over 4
+
+=item loglevel perspective
+
+Your program should probably be set to look at the loglevel
+B<$Log::Log4perl::ERROR> most of the time, so the user can avoid worrying
+about things that he/she doesn't care about.  But it should be fairly easy
+for the user to adjust this, in case more information would be helpful.  Use
+B<more_logging ()> to adjust the thresholds.
+
+=item code-writing perspective
+
+If there's even a slight chance that we can use information at a logging level,
+print it at some level - generally at B<all ()> or B<debug ()>.  B<info ()> is
+used for situations where we want to watch the general flow of the program, but
+not the details; B<warn ()> is for situations where something weird happened,
+but the user doesn't necessarily need to know that; B<error ()> is for when
+something bad has happened and the user really ought to know; and B<fatal ()>
+is for when we're going to die anyway.
+
+=back
+
+Some efficiency can be gained by using the B<is_level ()> functions, such as:
+
+    $logger->is_warn ();     # True if warning messages would go through
+
+Additionally, B<Log::Log4perl::Logger> offers functions that will log a message
+at the appropriate level and then warn/die/carp/whatever:
+
+    $logger->logwarn    ()
+    $logger->logdie     ()
+    $logger->logcarp    ()
+    $logger->logcroak   ()
+    $logger->logcluck   ()
+    $logger->logconfess ()
+    $logger->error_warn ()
+    $logger->error_warn ()
+
+Behind the scenes, B<Log::Log4perl> uses B<Log::Log4perl::Appender> objects to
+actually output the data from the B<Log::Log4perl::Logger> object.  We define
+two: a 'screen' appender, which logs to STDERR (colored by loglevel), and a
+'file' appender, which writes to a central logfile (no colors).  There are many
+more appender types than this, and we may take advantage of them in the future.
+
+At the appender level, there is a concept of thresholds - that is, we won't
+actually print the output to the appender if the message is above a certain
+priority.  Otherwise, the loglevel is set globally for the logger.  This is
+useful for the case where the user wants more debugging information on STDERR,
+but we don't want that extra information filling the logfile.
+
+You may note that B<Log::Log4perl>'s docs are very much centered on the
+"configuration file" initialization schemes.  I have attempted to ignore this
+where possible, and handle everything in a fairly object-oriented manner; but
+the default methods may be more appropriate, and are certainly more powerful.  
+So this decision may be revisited later.
+
+You may also note that there are all sorts of other issues to worry about, such as
+whether to print duplicate copies of a 'die' call at the script-level, or what
+the actual format of the log messages is.  I've tried to set sensible defaults
+and leave it at that; please refer to the man pages for more details.
+
+=head1 REQUIREMENTS
+
+B<Log::Log4perl>
 
 =head1 SEE ALSO
 
-Remedy::Config(8)
+Remedy::Config(8), Log::Log4perl::FAQ(8)
 
 =head1 HOMEPAGE
 
